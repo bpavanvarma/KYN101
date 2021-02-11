f290a036bad224b3a884a6ee37e7a0581087c584
Pavan Varma <bpavanvarma17@gmail.com>
2021-02-11 23:53:15 +0530

    port update

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 const app = express();
 
 const server = http.createServer(app);
